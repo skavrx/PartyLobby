9c19739ec07aae9033047605151da23a47c06889
xXJarJarXx <598Johnn897@gmail.com>
2014-07-24 16:36:53 -0400

    mhmmmm

---
+++ src/project/party/PartyLobby.java
@@ -0,0 +1,48 @@
+/**
+ * @author 598Johnn897
+ *
+ * Created: Jul 24, 2014
+ * Time: 4:34:33 PM
+ * Year: 2014
+ *
+ * By: 598Johnn897
+ * 
+ * Project: PartyLobby
+ * File: PartyLobby.java
+ * Package: project.party
+ */
+package project.party;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author 598Johnn897
+ *
+ * Created: Jul 24, 2014
+ * Time: 4:34:33 PM
+ * Year: 2014
+ *
+ * By: 598Johnn897
+ * 
+ * Project: PartyLobby
+ * File: PartyLobby.java
+ * Package: project.party
+ */
+public class PartyLobby extends JavaPlugin {
+	
+	@Override
+	public void onLoad() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
